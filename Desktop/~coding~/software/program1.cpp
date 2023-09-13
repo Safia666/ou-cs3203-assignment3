c908cc7ec9bc5f0cd951980ad0ad9aed3942d72b
Safia666 <69086140+Safia666@users.noreply.github.com>
2023-09-13 00:51:49 -0500

    added main function with sum and product output

--- Desktop/~coding~/software/program1.cpp
+++ Desktop/~coding~/software/program1.cpp
@@ -18,10 +18,40 @@ int productArray(int arr[], int size)
     int product = 0;
     for(int i = 0; i < size; i++)
     {
-       product *= arr[i];
+        if (i ==0)
+            product = 1;
+        product = product * arr[i];
     }
     return product;
 }
+
+int main()
+{
+    int size;
+    
+    cout << "How many numbers would you like to input?" << endl;
+    cin >> size;
+    int arr[size];
+
+
+    cout << "Input your numbers" << endl;
+    for (int i = 0; i < size; i ++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Sum of array: " << sumArray(arr, size) << endl;
+    cout << "Product of array: " << productArray(arr,size) << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
 
 
 
